fac8189780843b409148b797bd3e969919d94342
Claire Rehfuss <crehfuss@Claires-MacBook-Pro-2.local>
2017-12-06 17:52:59 -0500

    Added translation

--- get_bounding_boxes.py
+++ get_bounding_boxes.py
@@ -1,16 +1,31 @@
 ## Claire Rehfuss
 ## Made for EECS 442 - Computer Vision
 import matplotlib
+import Tkinter as tk
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+from Tkinter import *
+from tkFileDialog import *
 from scipy.misc import imread
+from scipy.misc import imresize
 from matplotlib.widgets import Button
+from predict import predict_char
+from PIL import Image
+import numpy as np
+import math
+from googletrans import Translator
+
+def get_label_name(label):
+    if label < 10:
+        return str(label)
+    if label < 36:
+        return chr(ord('A') - 10 + label)
+    return chr(ord('a') - 36 + label)
 
 class Annotate(object):
     def __init__(self, uploaded_image):
+        self.uploaded_image = uploaded_image
         plt.imshow(uploaded_image, zorder = 0)
         self.ax = plt.gca()
         axcut = plt.axes([0.9, 0.0, 0.1, 0.075])
@@ -45,8 +60,59 @@ class Annotate(object):
     # recognize the objects
     # Then closes all the matplotlib objects that are open
     def submit(self,event):
-        print(self.boundedboxes)
-        #plt.close('all')
+
+        word = ""
+        count = 0
+        cropped_images = np.empty(shape=(4, 32, 32, 3))
+        for i in self.boundedboxes:
+            if i[1]>1:
+                cropped_images[count,:,:,:] = crop_image(self.uploaded_image, i)
+                count +=1
+
+        guesses = predict_char(cropped_images)
+        for i in guesses:
+            word += get_label_name(i)
+
+        Translate(word)
+
+# run "pip install googletrans"
+class Translate(object):
+    def __init__(self,word):
+        self.word = word
+        self.root = tk.Tk()
+        # use width x height + x_offset + y_offset (no spaces!)
+        self.root.geometry("%dx%d+%d+%d" % (330, 80, 200, 150))
+        self.root.title("tk.Optionmenu as combobox")
+        self.var = tk.StringVar(self.root)
+        self.language = "English"
+        # initial value
+        self.var.set('Spanish')
+        self.choices = ['Spanish', 'French', 'Dutch', 'Russian', 'German', 'Thai']
+        self.dictionary = ['es', 'fr', 'nl', 'ru', 'de', 'th']
+        option = tk.OptionMenu(self.root, self.var, *self.choices)
+        option.pack(side='left', padx=10, pady=10)
+        button = tk.Button(self.root, text="check value slected", command=self.select)
+        button.pack(side='left', padx=20, pady=10)
+        self.root.mainloop()
+
+    def select(self,):
+        sf = "value is %s" % self.var.get()
+        self.root.title(sf)
+        # optional
+        self.language = self.var.get()
+        self.translate_word()
+
+    def translate_word(self,):
+        translator = Translator()
+        i = self.choices.index(self.language)
+        translation = translator.translate(self.word, dest=self.dictionary[i], src='en')
+        print translation.origin + " -> " +translation.text
+
+def crop_image(image, x_y_values):
+    cropped_img = image[int(math.ceil(x_y_values[1])):int(math.ceil(x_y_values[3])), int(math.ceil(x_y_values[0])):int(math.ceil(x_y_values[2])), :]
+    cropped_img = imresize(cropped_img, (32, 32))
+    cropped_img = cropped_img[:, :, :3]
+    return cropped_img
 
 def main():
     """Create our Master class instantiation."""
@@ -60,7 +126,6 @@ def main():
 
     a = Annotate(img)
     plt.show()
-
 
 
 if __name__ == '__main__':
